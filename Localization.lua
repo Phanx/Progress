41b220961663cc4b6a6186daa086a47a28943d33
Andrew Mordecai <am@armord.net>
2011-03-16 00:08:29 +0000

    Added German and French translations

--- Localization.lua
+++ Localization.lua
@@ -17,22 +17,21 @@ ns.L = L
 
 --[[--------------------------------------------------------------------
 	deDE | Deutsch | German
-	Last updated: YYYY-MM-DD by UNKNOWN < CONTACT INFO >
+	Last updated: 2011-03-14 by Akkorian
 ----------------------------------------------------------------------]]
 
 if GAME_LOCALE == "deDE" then
---	L["Progress"] = ""
---	L["Current XP"] = ""
---	L["Rested XP"] = ""
---	L["XP To Next Level"] = ""
---	L["XP To Level %d"] = ""
---	L["To Next Standing"] = ""
---	L["Click to toggle the reputation panel."] = ""
+	L["Progress"] = "Fortschritt"
+	L["Current XP"] = "Aktuelle EP"
+	L["Rested XP"] = "Erholt EP"
+	L["XP To Next Level"] = "EP zum nächsten Stufe"
+	L["XP To Level %d"] = "EP zum Stufe 85"
+	L["To Next Standing"] = "Ruf zum nächsten Einstellung"
+	L["Click to toggle the reputation panel."] = "Klicken, um den Ruffenster anzuzeigen."
 return end
 
 --[[--------------------------------------------------------------------
-	esES | Español (EU) | Spanish (Europe)
-	esMX | Español (AL) | Spanish (Latin America)
+	esES/esMX | Español (EU/AL) | Spanish (Europe/Latin America)
 	Last updated: 2010-12-23 by Akkorian
 ----------------------------------------------------------------------]]
 
@@ -43,22 +42,22 @@ if GAME_LOCALE == "esES" or GetLocale() == "esMX" then
 	L["XP To Next Level"] = "PE al siguiente nivel"
 	L["XP To Level %d"] = "PE al nivel %d"
 	L["To Next Standing"] = "Al siguiente prestigio"
-	L["Click to toggle the reputation panel."] = "Haz clic para mostrar/ocultar\nel panel de reputación."
+	L["Click to toggle the reputation panel."] = "Haz clic para abrir el panel de reputación."
 return end
 
 --[[--------------------------------------------------------------------
 	frFR | Français | French
-	Last updated: YYYY-MM-DD by UNKNOWN < CONTACT INFO >
+	Last updated: 2011-03-14 by Akkorian
 ----------------------------------------------------------------------]]
 
 if GAME_LOCALE == "frFR" then
---	L["Progress"] = ""
---	L["Current XP"] = ""
---	L["Rested XP"] = ""
---	L["XP To Next Level"] = ""
---	L["XP To Level %d"] = ""
---	L["To Next Standing"] = ""
---	L["Click to toggle the reputation panel."] = ""
+	L["Progress"] = "Progrès"
+	L["Current XP"] = "PX actuelle"
+	L["Rested XP"] = "PX reposée"
+	L["XP To Next Level"] = "PX au niveau suivant"
+	L["XP To Level %d"] = "PX au niveau %d"
+	L["To Next Standing"] = "Rép. au statut supérieur"
+	L["Click to toggle the reputation panel."] = "Cliquez pour afficher la feuille de réputation."
 return end
 
 --[[--------------------------------------------------------------------
@@ -70,7 +69,7 @@ if GAME_LOCALE == "ruRU" then
 	L["Progress"] = "Прогресс"
 	L["Current XP"] = "Текущий опыт"
 	L["Rested XP"] = "Отдых опыт"
-	L["XP To Next Level"] = "к следующий уровень"
+	L["XP To Next Level"] = "к следующий уровня"
 	L["XP To Level %d"] = "к %d-го уровня"
 	L["To Next Standing"] = "к следующий отношения"
 	L["Click to toggle the reputation panel."] = "Щелкните, чтобы открыть окно репутации."
@@ -78,7 +77,7 @@ return end
 
 --[[--------------------------------------------------------------------
 	koKR | 한국어 | Korean
-	Last updated: YYYY-MM-DD by UNKNOWN < CONTACT INFO >
+	Last updated: YYYY-MM-DD by UNKNOWN
 ----------------------------------------------------------------------]]
 
 if GAME_LOCALE == "koKR" then
@@ -93,7 +92,7 @@ return end
 
 --[[--------------------------------------------------------------------
 	zhCN | 简体中文 | Simplified Chinese
-	Last updated: YYYY-MM-DD by UNKNOWN < CONTACT INFO >
+	Last updated: YYYY-MM-DD by UNKNOWN
 ----------------------------------------------------------------------]]
 
 if GAME_LOCALE == "zhCN" then
@@ -107,8 +106,8 @@ if GAME_LOCALE == "zhCN" then
 return end
 
 --[[--------------------------------------------------------------------
-	zhTW | 正體中文 | Traditional Chinese
-	Last updated: YYYY-MM-DD by UNKNOWN < CONTACT INFO >
+	zhTW | 繁體中文 | Traditional Chinese
+	Last updated: YYYY-MM-DD by UNKNOWN
 ----------------------------------------------------------------------]]
 
 if GAME_LOCALE == "zhTW" then
