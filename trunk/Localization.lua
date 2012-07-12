0280c577218568a21c06f102724d6db6668a6992
Phanx-28751 <Phanx-28751@5eda4ab8-76dc-48ee-9f3b-8c5b740778a0>
2012-07-12 09:32:54 +0000

    - Simplified license - Updated README - Added beta tag to TOC for MoP development

--- trunk/Localization.lua
+++ trunk/Localization.lua
@@ -1,8 +1,8 @@
 ﻿--[[--------------------------------------------------------------------
 	Progress
 	DataBroker plugin to show experience and reputation.
-	Written by Phanx <addons@phanx.net>
-	Copyright © 2008–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
+	Copyright (c) 2008-2012 Phanx <addons@phanx.net>. All rights reserved.
+	See the accompanying README and LICENSE files for more information.
 	http://www.wowinterface.com/downloads/info11032-Progress.html
 	http://www.curse.com/addons/wow/progress
 ----------------------------------------------------------------------]]
@@ -57,6 +57,21 @@ if GAME_LOCALE == "frFR" then
 	L["XP To Level %d"] = "PX au niveau %d"
 	L["To Next Standing"] = "Rép. au statut supérieur"
 	L["Click to toggle the reputation panel."] = "Cliquez pour afficher la feuille de réputation."
+return end
+
+--[[--------------------------------------------------------------------
+	itIT | Italian | Italiano
+	Last updated: YYYY-MM-DD by UNKNOWN
+----------------------------------------------------------------------]]
+
+if GAME_LOCALE == "itIT" then
+--	L["Progress"] = ""
+--	L["Current XP"] = ""
+--	L["Rested XP"] = ""
+--	L["XP To Next Level"] = ""
+--	L["XP To Level %d"] = ""
+--	L["To Next Standing"] = ""
+--	L["Click to toggle the reputation panel."] = ""
 return end
 
 --[[--------------------------------------------------------------------
