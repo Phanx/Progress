981d41f9ff1dcca6122ff03a47a2a66d35a9a490
Phanx-28751 <Phanx-28751@5eda4ab8-76dc-48ee-9f3b-8c5b740778a0>
2012-07-28 22:56:41 +0000

    - Moved some stuff around - Better localization support in digit grouping

--- trunk/Progress.lua
+++ trunk/Progress.lua
@@ -75,87 +75,88 @@ local STANDING_LABEL_FEMALE = {
 
 ------------------------------------------------------------------------
 
-local XP_PER_LEVEL = { -- How much XP needed to complete each level
+local XP_PER_LEVEL = { -- How much XP needed to reach each level
+	0, -- 1
 	400,
 	900,
 	1400,
-	2100,
-	2800, -- 5
+	2100, -- 5
+	2800,
 	3600,
 	4500,
 	5400,
-	6500,
-	7600, -- 10
+	6500, -- 10
+	7600,
 	8700,
 	9800,
 	11000,
-	12300,
-	13600, -- 15
+	12300, -- 15
+	13600,
 	15000,
 	16400,
 	17800,
-	19300,
-	20800, -- 20
+	19300, -- 20
+	20800,
 	22400,
 	24000,
 	25500,
-	27200,
-	28900, -- 25
+	27200, -- 25
+	28900,
 	30500,
 	32200,
 	33900,
-	36300,
-	38800, -- 30
+	36300, -- 30
+	38800,
 	41600,
 	44600,
-	48000,
+	48000, -- 35
 	51400,
-	55000, -- 35
+	55000,
 	58700,
 	62400,
 	66200,
-	70200,
-	74300, -- 40
+	70200, -- 40
+	74300,
 	78500,
 	82800,
 	87100,
-	91600,
-	96300, -- 45
+	91600, -- 45
+	96300,
 	101000,
 	105800,
 	110700,
-	115700,
-	120900, -- 50
+	115700, -- 50
+	120900,
 	126100,
 	131500,
 	137000,
-	142500,
-	148200, -- 55
+	142500, -- 55
+	148200,
 	154000,
 	159900,
 	165800,
-	172000,
-	290000, -- 60
+	172000, -- 60
+	290000,
 	317000,
 	349000,
 	386000,
-	428000,
-	475000, -- 65
+	428000, -- 65
+	475000,
 	527000,
 	585000,
 	658000,
-	717000,
-	1523800, -- 70
+	717000, -- 70
+	1523800,
 	1539600,
 	1555700,
 	1571800,
-	1587900,
-	1604200, -- 75
+	1587900, -- 75
+	1604200,
 	1620700,
 	1637400,
 	1653900,
-	1670800,
-	1686300, -- 80
+	1670800, -- 80
+	1686300,
 	2121500,
 	4004000,
 	5203400,
@@ -164,16 +165,8 @@ local XP_PER_LEVEL = { -- How much XP needed to complete each level
 
 ------------------------------------------------------------------------
 
-local MAX_LEVEL = MAX_PLAYER_LEVEL_TABLE[GetAccountExpansionLevel()]
-if MAX_LEVEL - 1 > #XP_PER_LEVEL then
-	-- temp workaround for MoP beta
-	MAX_LEVEL = #XP_PER_LEVEL + 1
-end
-
+local MAX_PLAYER_LEVEL
 local XP_TO_MAX_LEVEL = 0
-for i = 1, MAX_LEVEL - 1 do
-	XP_TO_MAX_LEVEL = XP_TO_MAX_LEVEL + XP_PER_LEVEL[i]
-end
 
 local xpToCurrentLevel = 0
 local shortFactions = { }
@@ -199,74 +192,6 @@ local function Debug(lvl, str, ...)
 end
 
 ------------------------------------------------------------------------
-
-local groupmark = "%1" .. ( ( GetLocale():match("^en") or GetLocale() == "koKR" ) and "," or " " )
-
-local function GroupDigits(num)
-	if not num then return 0 end
-	if abs(num) < 10000 then return num end
-
-	local neg = num < 0 and "-" or ""
-	local left, mid, right = tostring(abs(num)):match("^([^%d]*%d)(%d*)(.-)$")
-	return ("%s%s%s%s"):format(neg, left, mid:reverse():gsub("(%d%d%d)", groupmark):reverse(), right)
-end
-
-------------------------------------------------------------------------
-
-function Progress:UpdateText()
-	Debug(2, "UpdateText")
-	if UnitLevel("player") < MAX_LEVEL then
-		local cur, max = UnitXP("player"), UnitXPMax("player")
-		self.obj.text = ("%s (%d%%)"):format(GroupDigits(max - cur), math.floor(cur / max * 100 + 0.5))
-	else
-		local name, standing, min, max, cur = GetWatchedFactionInfo()
-		if name then
-			self.obj.text = ("%s%s (%d%%)|r"):format(STANDING_COLOR[standing], GroupDigits((max - min) - (cur - min)), math_floor((cur - min) / (max - min) * 100))
-		else
-			self.obj.text = L["Progress"]
-		end
-	end
-end
-
-function Progress:UpdateTooltip(tooltip)
-	tooltip:AddLine(L["Progress"], 1, 1, 1)
-	tooltip:AddLine(" ")
-
-	local needblank
-	local myLevel = UnitLevel("player")
-	if myLevel < MAX_LEVEL then
-		tooltip:AddDoubleLine(L["Level"], myLevel, nil, nil, nil, 1, 1, 1)
-		if myLevel < MAX_LEVEL then
-			local cur, max, rest = UnitXP("player"), UnitXPMax("player"), GetXPExhaustion()
-			tooltip:AddDoubleLine(L["Current XP"], ("%s / %s (%d%%)"):format(GroupDigits(cur), GroupDigits(max), math_floor(cur / max * 100 + 0.5)), nil, nil, nil, 1, 1, 1)
-			if rest then
-				tooltip:AddDoubleLine(L["Rested XP"], ("%s (%s%%)"):format(GroupDigits(rest), math_floor(rest / max * 1000 / 10 + 0.5)), nil, nil, nil, 1, 1, 1)
-			end
-			tooltip:AddDoubleLine(L["XP To Next Level"], GroupDigits(max - cur), nil, nil, nil, 1, 1, 1)
-			tooltip:AddLine(" ")
-			tooltip:AddDoubleLine(L["XP To Level %d"]:format(MAX_LEVEL), GroupDigits(XP_TO_MAX_LEVEL - (xpToCurrentLevel + cur)), nil, nil, nil, 1, 1, 1)
-		end
-		needblank = true
-	end
-	if myLevel == MAX_LEVEL or self.db.forceRep then
-		local name, standing, min, max, cur = GetWatchedFactionInfo()
-		if name then
-			if needblank then
-				tooltip:AddLine(" ")
-				needblank = nil
-			end
-			tooltip:AddDoubleLine(L["Faction"], name, nil, nil, nil, 1, 1, 1)
-			tooltip:AddDoubleLine(L["Standing"], format("%s%s|r", STANDING_COLOR[standing], UnitSex("player") == 3 and STANDING_LABEL_FEMALE[standing] or STANDING_LABEL_MALE[standing]))
-			tooltip:AddDoubleLine(L["Reputation"], format("%s / %s (%d%%)", GroupDigits(cur - min), GroupDigits(max - min), math_floor(cur / max * 100 + 0.5)), nil, nil, nil, 1, 1, 1)
-			if standing < 8 then
-				tooltip:AddDoubleLine(L["To Next Standing"], GroupDigits(max - cur), nil, nil, nil, 1, 1, 1)
-			end
-		end
-	end
-	tooltip:AddLine(" ")
-	tooltip:AddLine(L["Click to toggle the reputation panel."])
-	tooltip:Show()
-end
 
 ------------------------------------------------------------------------
 
@@ -296,6 +221,18 @@ function Progress:ADDON_LOADED(addon)
 end
 
 function Progress:PLAYER_LOGIN()
+	MAX_PLAYER_LEVEL = MAX_PLAYER_LEVEL_TABLE[GetExpansionLevel()]
+
+	for i = 1, MAX_PLAYER_LEVEL do
+		local xp = XP_PER_LEVEL[i]
+		if not xp then
+			-- temp workaround for new expansions without data yet
+			xp = XP_PER_LEVEL[i-1]
+			XP_PER_LEVEL[i] = xp
+		end
+		XP_TO_MAX_LEVEL = XP_TO_MAX_LEVEL + xp
+	end
+
 	self.obj = LibStub("LibDataBroker-1.1"):NewDataObject("Progress", {
 		type  = "data source",
 		icon  = "Interface\\Icons\\INV_Misc_PocketWatch_02",
@@ -307,7 +244,7 @@ function Progress:PLAYER_LOGIN()
 
 	self:PLAYER_LEVEL_UP()
 
-	if UnitLevel("player") < MAX_LEVEL then
+	if UnitLevel("player") < MAX_PLAYER_LEVEL then
 		self:RegisterEvent("PLAYER_XP_UPDATE")
 		self:RegisterEvent("UPDATE_EXHAUSTION")
 	else
@@ -337,7 +274,7 @@ end
 
 function Progress:PLAYER_LEVEL_UP()
 	local level = UnitLevel("player")
-	if level == MAX_LEVEL then
+	if level == MAX_PLAYER_LEVEL then
 		xpToCurrentLevel = XP_TO_MAX_LEVEL
 		self:RegisterEvent("UPDATE_FACTION")
 		self:UnregisterEvent("PLAYER_LEVEL_UP")
@@ -352,4 +289,72 @@ function Progress:PLAYER_LEVEL_UP()
 			end
 		end
 	end
+end
+
+------------------------------------------------------------------------
+
+local groupmark = "%1" .. (tonumber("1.1") and "." or ",")
+
+local function GroupDigits(num)
+	if not num then return 0 end
+	if abs(num) < 10000 then return num end
+
+	local neg = num < 0 and "-" or ""
+	local left, mid, right = tostring(abs(num)):match("^([^%d]*%d)(%d*)(.-)$")
+	return ("%s%s%s%s"):format(neg, left, mid:reverse():gsub("(%d%d%d)", groupmark):reverse(), right)
+end
+
+function Progress:UpdateText()
+	Debug(2, "UpdateText")
+	if UnitLevel("player") < MAX_PLAYER_LEVEL then
+		local cur, max = UnitXP("player"), UnitXPMax("player")
+		self.obj.text = ("%s (%d%%)"):format(GroupDigits(max - cur), math.floor(cur / max * 100 + 0.5))
+	else
+		local name, standing, min, max, cur = GetWatchedFactionInfo()
+		if name then
+			self.obj.text = ("%s%s (%d%%)|r"):format(STANDING_COLOR[standing], GroupDigits((max - min) - (cur - min)), math_floor((cur - min) / (max - min) * 100))
+		else
+			self.obj.text = L["Progress"]
+		end
+	end
+end
+
+function Progress:UpdateTooltip(tooltip)
+	tooltip:AddLine(L["Progress"], 1, 1, 1)
+	tooltip:AddLine(" ")
+
+	local needblank
+	local myLevel = UnitLevel("player")
+	if myLevel < MAX_PLAYER_LEVEL then
+		tooltip:AddDoubleLine(L["Level"], myLevel, nil, nil, nil, 1, 1, 1)
+		if myLevel < MAX_PLAYER_LEVEL then
+			local cur, max, rest = UnitXP("player"), UnitXPMax("player"), GetXPExhaustion()
+			tooltip:AddDoubleLine(L["Current XP"], ("%s / %s (%d%%)"):format(GroupDigits(cur), GroupDigits(max), math_floor(cur / max * 100 + 0.5)), nil, nil, nil, 1, 1, 1)
+			if rest then
+				tooltip:AddDoubleLine(L["Rested XP"], ("%s (%s%%)"):format(GroupDigits(rest), math_floor(rest / max * 1000 / 10 + 0.5)), nil, nil, nil, 1, 1, 1)
+			end
+			tooltip:AddDoubleLine(L["XP To Next Level"], GroupDigits(max - cur), nil, nil, nil, 1, 1, 1)
+			tooltip:AddLine(" ")
+			tooltip:AddDoubleLine(L["XP To Level %d"]:format(MAX_PLAYER_LEVEL), GroupDigits(XP_TO_MAX_LEVEL - (xpToCurrentLevel + cur)), nil, nil, nil, 1, 1, 1)
+		end
+		needblank = true
+	end
+	if myLevel == MAX_PLAYER_LEVEL or self.db.forceRep then
+		local name, standing, min, max, cur = GetWatchedFactionInfo()
+		if name then
+			if needblank then
+				tooltip:AddLine(" ")
+				needblank = nil
+			end
+			tooltip:AddDoubleLine(L["Faction"], name, nil, nil, nil, 1, 1, 1)
+			tooltip:AddDoubleLine(L["Standing"], format("%s%s|r", STANDING_COLOR[standing], UnitSex("player") == 3 and STANDING_LABEL_FEMALE[standing] or STANDING_LABEL_MALE[standing]))
+			tooltip:AddDoubleLine(L["Reputation"], format("%s / %s (%d%%)", GroupDigits(cur - min), GroupDigits(max - min), math_floor(cur / max * 100 + 0.5)), nil, nil, nil, 1, 1, 1)
+			if standing < 8 then
+				tooltip:AddDoubleLine(L["To Next Standing"], GroupDigits(max - cur), nil, nil, nil, 1, 1, 1)
+			end
+		end
+	end
+	tooltip:AddLine(" ")
+	tooltip:AddLine(L["Click to toggle the reputation panel."])
+	tooltip:Show()
 end
